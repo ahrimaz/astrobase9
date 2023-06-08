d7442261e9fcddd6ed82838cb5872979d12befc1
ahrimaz <eric.stanard@gmail.com>
2023-06-08 17:24:44 -0400

    font variables

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -15,7 +15,7 @@ export default function Nav({ user }:Session){
     return(
         <nav className='flex justify-between items-center py-12'>
             <Link href={'/'}>
-            <h1 className='font-extrabold'>astrobase9</h1>
+            <h1 className='font-roboto text-xl font-extrabold'>astrobase9</h1>
             </Link>
             <ul className='flex items-center gap-8'>
                 {/* toggle cart state */}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import Nav from './components/Nav'
 import {getServerSession} from 'next-auth/next'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import Hydrate from './components/Hydrate'
-import { Roboto } from 'next/font/google'
+import { Roboto, Lobster_Two } from 'next/font/google'
 
 //main font
-const roboto = Roboto({weight: ['400','500','700'], subsets: ['latin']})
+const roboto = Roboto({weight: ['400','500','700'], subsets: ['latin'], variable: '--font-robot'})
+const lobster = Lobster_Two({weight: '700', subsets: ['latin'], variable: '--font-lobster'})
 
 export const metadata = {
   title: 'astrobase9',
@@ -21,7 +22,7 @@ export default async function RootLayout({
   //fetch the user information
   const session = await getServerSession(authOptions)
   return (
-    <html lang="en">
+    <html className={`${roboto.variable} ${lobster.variable}`} lang="en">
         <Hydrate>
           <Nav user={session?.user} expires={session?.expires as string}/>
             {children}
